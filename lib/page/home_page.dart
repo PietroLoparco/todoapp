5c88e81b44a04ef572b9c7886d9b2cff046dc3e1
Pietro <pietroloparco010203@gmail.com>
2023-06-24 06:46:28 +0200

    Changes to be committed: 	modified:   lib/page/home_page.dart

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+ 
   @override
   State<HomePage> createState() => _HomePageState();
 }
